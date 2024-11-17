bb0697f8c5c4bc630a70db69b635e5afd232c734
Satriooo <satriocahyo93@gmail.com>
2024-11-17 16:38:03 +0700

    Add theme landing

--- app/src/main/java/com/exal/testapp/view/landing/LandingActivity.kt
+++ app/src/main/java/com/exal/testapp/view/landing/LandingActivity.kt
@@ -1,11 +1,7 @@
 package com.exal.testapp.view.landing
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.exal.testapp.R
 import com.exal.testapp.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
@@ -17,13 +13,5 @@ class LandingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLandingBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_landing)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
