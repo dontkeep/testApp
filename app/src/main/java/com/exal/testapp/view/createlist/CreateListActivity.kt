21748e8eeb33cfe9658181cbb050f2849a706ace
Satriooo <satriocahyo93@gmail.com>
2024-11-21 18:18:35 +0700

    Add CreateList.

---
+++ app/src/main/java/com/exal/testapp/view/createlist/CreateListActivity.kt
@@ -0,0 +1,21 @@
+package com.exal.testapp.view.createlist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.exal.testapp.R
+
+class CreateListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
