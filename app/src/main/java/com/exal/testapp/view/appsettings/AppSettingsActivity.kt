21f91252a22391d4cb23b6b5d4349bc726b5eab4
dontkeep <alsdiaryy@gmail.com>
2024-11-26 18:09:09 +0700

    Added : - App Settings Activity(Base) - Adjust text, icon, and padding size - Added Plan Fragment and Material Design Calendar

---
+++ app/src/main/java/com/exal/testapp/view/appsettings/AppSettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.exal.testapp.view.appsettings
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.exal.testapp.R
+
+class AppSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_app_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/exal/testapp/view/plan/PlanFragment.kt
+++ app/src/main/java/com/exal/testapp/view/plan/PlanFragment.kt
@@ -6,26 +6,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.exal.testapp.R
+import com.exal.testapp.databinding.FragmentExpensesBinding
+import com.exal.testapp.databinding.FragmentPlanBinding
+import com.exal.testapp.helper.MonthYearPickerDialog
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatterBuilder
+import java.util.Locale
 
+@AndroidEntryPoint
 class PlanFragment : Fragment() {
+    private var _binding: FragmentPlanBinding? = null
+    private val binding get() = _binding!!
 
-    companion object {
-        fun newInstance() = PlanFragment()
-    }
-
-    private val viewModel: PlanViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_plan, container, false)
+        _binding = FragmentPlanBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val format = SimpleDateFormat("dd MMMM yyyy", Locale("in", "ID"))
+
+        binding.icCalender.setOnClickListener {
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Select date")
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build()
+
+            datePicker.show(parentFragmentManager, "DatePicker")
+
+            datePicker.addOnPositiveButtonClickListener {
+                Toast.makeText(requireContext(), format.format(it), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Avoid memory leaks
     }
 }
