5500e460cbba4d8a610dfc000ad16ab6e1a6adf4
Satriooo <satriocahyo93@gmail.com>
2024-11-30 00:37:10 +0700

    Completed help button.

--- app/src/main/java/com/exal/testapp/view/landing/LandingActivity.kt
+++ app/src/main/java/com/exal/testapp/view/landing/LandingActivity.kt
@@ -4,10 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.exal.testapp.databinding.ActivityLandingBinding
+import com.exal.testapp.helper.manager.IntroManager
+import com.exal.testapp.view.intro.IntroActivity
 import com.exal.testapp.view.login.LoginActivity
 import com.exal.testapp.view.register.RegisterActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LandingActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var introManager: IntroManager
 
     private var binding: ActivityLandingBinding? = null
     private val _binding get() = binding!!
@@ -24,6 +32,12 @@ class LandingActivity : AppCompatActivity() {
 
         _binding.registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        _binding.helpBtn.setOnClickListener {
+            introManager.clearIntroFlag()
+            val intent = Intent(this, IntroActivity::class.java)
             startActivity(intent)
         }
     }
